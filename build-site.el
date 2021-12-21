11257c9597de7468fbb7d39c49280a2d81936a39
Nahuel J. Sacchetti <me@nsacchetti.com>
2021-12-20 19:36:02 -0600

    Initial commit

---
+++ build-site.el
@@ -0,0 +1,37 @@
+(require 'package)
+(setq package-user-dir (expand-file-name "./.packages"))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+;; Initialize the package system
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Install dependencies
+(package-install 'htmlize)
+
+(require 'ox-publish)
+
+(setq org-html-validation-link nil
+      org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+      org-html-head
+      "<link rel=\"stylesheet\" href=\"./css/styles.css\" />")
+
+(setq org-publish-project-alist
+      (list
+       (list "org-site:main"
+             :recursive t
+             :base-directory "./content"
+             :publishing-function 'org-html-publish-to-html
+             :publishing-directory "./public"
+             :with-author nil
+             :with-creator t
+             :with-toc t
+             :section-numbers nil
+             :time-stamp-file nil)))
+
+(org-publish-all t)
+
+(message "BUILD FINISHED")
